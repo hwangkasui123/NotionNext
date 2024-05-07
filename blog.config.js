7f32b40d38dcec13969cf3612e533d385c8eff5e
hwangkasui123 <168792236+hwangkasui123@users.noreply.github.com>
2024-05-07 19:18:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Dororo又如何', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '每天只想着混吃等死打游戏', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://dororodoujou.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://dororodoujo.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
